42876749352f0853eeddf1d00296c316778c2769
Kevin Dass <kdass@mast.com>
2023-03-02 12:20:56 +0000

    first commit

---
+++ list_page/webapp/controller/App.controller.js
@@ -0,0 +1,14 @@
+sap.ui.define(
+    [
+        "sap/ui/core/mvc/Controller"
+    ],
+    function(BaseController) {
+      "use strict";
+  
+      return BaseController.extend("com.lb.flp.tracker.listpage.controller.App", {
+        onInit() {
+        }
+      });
+    }
+  );
+  
---
+++ list_page/webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"comlbflptracker/list_page/test/unit/controller/View1.controller"
+], function () {
+	"use strict";
+});
